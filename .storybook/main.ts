d5f7e6017d3e6d6747db5755add5f4421c62f2bb
yogarasu <ishiguro@revoltage.jp>
2022-08-30 19:47:22 +0900

    fix #5 Storybookのmain.tsをesbuildでラップするように修正

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,12 @@
-module.exports = {
+import { StorybookConfig } from '@storybook/core-common'
+import { mergeConfig } from 'vite'
+import { join } from 'path'
+
+interface Config extends StorybookConfig {
+  viteFinal?: (config: Record<string, any>) => Promise<Record<string, any>>
+}
+
+const config: Config = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
@@ -10,4 +18,19 @@ module.exports = {
     builder: '@storybook/builder-vite',
   },
   staticDirs: ['../public'],
+  async viteFinal(config: Record<string, any>) {
+    return mergeConfig(config, {
+      plugins: [],
+      resolve: {
+        alias: [],
+      },
+      // HACK: STORYBOOK environment variable is not working, so set
+      // https://github.com/storybookjs/storybook/pull/12997
+      define: {
+        'process.env.STORYBOOK': true,
+      },
+    })
+  },
 }
+
+export default config
