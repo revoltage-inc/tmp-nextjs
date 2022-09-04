20fce6c4c1a7cb13f343fdf6de21fbbbd6ca82bc
yogarasu <ishiguro@revoltage.jp>
2022-09-05 08:25:52 +0900

    fix #5 ViteでインラインSVGを表示できるように修正

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
-import { StorybookConfig } from '@storybook/react-webpack5'
-import { resolve } from 'path'
-import { DefinePlugin } from 'webpack'
+import { StorybookConfig } from '@storybook/react-vite'
+// import { StorybookConfig } from '@storybook/react-webpack5'
+// import { DefinePlugin } from 'webpack'
+// import { resolve } from 'path'
 
 const config: StorybookConfig = {
   stories: ['../src/components/**/*.stories.tsx'],
@@ -12,54 +13,54 @@ const config: StorybookConfig = {
   ],
   staticDirs: ['../public'],
   framework: {
-    // name: '@storybook/react-vite',
-    name: '@storybook/react-webpack5',
+    name: '@storybook/react-vite',
+    // name: '@storybook/react-webpack5',
     options: {
       fastRefresh: true,
       strictMode: true,
     },
   },
-  webpackFinal: async (config) => {
-    if (config.module?.rules) {
-      config.module.rules = config.module.rules.map((rule) => {
-        if (rule !== '...' && rule.oneOf) {
-          rule.oneOf = rule.oneOf.map((oneOfRule) => {
-            // HACK: Override SVG loader to not use file-loader
-            if (String(oneOfRule.test) === String(/\.svg$/)) {
-              return {
-                test: /\.svg$/,
-                use: ['@svgr/webpack'],
-                issuer: /\.tsx$/,
-              }
-            }
-            return oneOfRule
-          })
-        }
+  // webpackFinal: (config) => {
+  //   if (config.module?.rules) {
+  //     config.module.rules = config.module.rules.map((rule) => {
+  //       if (rule !== '...' && rule.oneOf) {
+  //         rule.oneOf = rule.oneOf.map((oneOfRule) => {
+  //           // HACK: Override SVG loader to not use file-loader
+  //           if (String(oneOfRule.test) === String(/\.svg$/)) {
+  //             return {
+  //               test: /\.svg$/,
+  //               use: ['@svgr/webpack'],
+  //               issuer: /\.tsx$/,
+  //             }
+  //           }
+  //           return oneOfRule
+  //         })
+  //       }
 
-        return rule
-      })
-    }
+  //       return rule
+  //     })
+  //   }
 
-    if (config.resolve?.alias) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        ...{
-          '@css': resolve(__dirname, '../src/assets/css/'),
-          '@svg': resolve(__dirname, '../src/assets/svg/'),
-        },
-      }
-    }
+  //   if (config.resolve?.alias) {
+  //     config.resolve.alias = {
+  //       ...config.resolve.alias,
+  //       ...{
+  //         '@css': resolve(__dirname, '../src/assets/css/'),
+  //         '@svg': resolve(__dirname, '../src/assets/svg/'),
+  //       },
+  //     }
+  //   }
 
-    // HACK: STORYBOOK environment variable is not working, so set
-    // https://github.com/storybookjs/storybook/pull/12997
-    config.plugins?.push(
-      new DefinePlugin({
-        STORYBOOK: true,
-      })
-    )
+  //   // HACK: STORYBOOK environment variable is not working, so set
+  //   // https://github.com/storybookjs/storybook/pull/12997
+  //   config.plugins?.push(
+  //     new DefinePlugin({
+  //       STORYBOOK: true,
+  //     })
+  //   )
 
-    return config
-  },
+  //   return config
+  // },
 }
 
 export default config
