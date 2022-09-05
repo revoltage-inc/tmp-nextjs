10a753c9f94bb93c5a17ae6b01728d7248e770a9
yogarasu <ishiguro@revoltage.jp>
2022-09-05 09:26:18 +0900

    fix #9 不要なパッケージを削除

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,6 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
   ],
   staticDirs: ['../public'],
   framework: {
